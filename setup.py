d4db21616be8f364aa3fb8330dee3f6255d285b0
SayedShaun <sayedshaun4@gmail.com>
2024-08-06 21:47:16 +0600

    updated

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import setuptools
 
-long_description = open("README.md", "r").read()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="pos_ner",
     version="1.0.0",
@@ -8,13 +10,20 @@ setuptools.setup(
     author_email="sayedshaun4@gmail.com",
     description="Part-Of-Speech and Named Entity Recognition System",
     long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SayedShaun/POS-NER.git",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
-    requires=[
-        'torch', 
-        'pandas', 
-        'matplotlib', 
-        'tqdm', 
+    install_requires=[
+        'torch',
+        'pandas',
+        'matplotlib',
+        'tqdm',
         'scikit-learn'
-        ]
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
