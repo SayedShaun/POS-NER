b61b1a9bec0d41bd3bff751a48fb0e57e0225c34
SayedShaun <sayedshaun4@gmail.com>
2024-08-06 21:41:45 +0600

    updated

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-# Setup 
 import setuptools
 
+long_description = open("README.md", "r").read()
 setuptools.setup(
     name="pos_ner",
     version="1.0.0",
     author="Sayed Shaun",
     author_email="sayedshaun4@gmail.com",
-    description="POS NER",
+    description="Part-Of-Speech and Named Entity Recognition System",
+    long_description=long_description,
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
+    requires=[
+        'torch', 
+        'pandas', 
+        'matplotlib', 
+        'tqdm', 
+        'scikit-learn'
+        ]
 )
